3eafc17fcd90906666b4ef1ad98338d43848df99
Shiv Kumar <championballer08@gmail.com>
2018-09-09 14:09:31 +0000

    added grader exercise

---
+++ introToNode/grader.js
@@ -0,0 +1,16 @@
+function average(scores)
+{
+    var avg = 0;
+    for(var i = 0 ;i<scores.length;i++)
+    {
+        avg+=scores[i];
+    }
+    
+    return avg/(scores.length);
+}
+
+var scores = [90,98,89,100,100,86,94];
+var scores2 = [40,65,77,82,80,54,73,63,95,49];
+
+console.log(average(scores));
+console.log(average(scores2));
