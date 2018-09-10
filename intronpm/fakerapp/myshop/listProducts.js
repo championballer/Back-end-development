f91b7f06cbcc86e5f9d84c4084da97c71d73b671
Shiv Kumar <championballer08@gmail.com>
2018-09-10 12:08:12 +0000

    completed fakerExercise

---
+++ intronpm/fakerapp/myshop/listProducts.js
@@ -0,0 +1,17 @@
+var faker = require('faker');
+
+
+var product = [];
+var prices = [];
+for(var i = 0;i<10;i++)
+{
+    product.push(faker.fake("{{commerce.productName}}"));
+    prices.push(faker.fake("{{commerce.price}}"));
+    
+    console.log(product[i]+":"+prices[i]);
+}
+
+
+
+
+
