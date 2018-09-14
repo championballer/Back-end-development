47aaf629f12a6dddedd504b0ce1075d54b7abd4f
Shiv Kumar <championballer08@gmail.com>
2018-09-14 17:08:39 +0000

    added * file

--- expressInit/app.js
+++ expressInit/app.js
@@ -17,6 +17,11 @@ app.get("/dog",function(req,res){
    console.log("dog request made");
 });
 
+app.get("*",function(req,res){
+    res.send("* encountered");
+    console.log("* found");
+});
+
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Server has started");
 });
