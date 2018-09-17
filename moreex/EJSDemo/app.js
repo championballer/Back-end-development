ed4552e046aa22cebf486e4f4f56d9cd74055e25
Shiv Kumar <championballer08@gmail.com>
2018-09-17 12:07:58 +0000

    ejs commits

--- moreex/EJSDemo/app.js
+++ moreex/EJSDemo/app.js
@@ -2,8 +2,13 @@ var express = require("express");
 var app = express();
 
 app.get("/",function(req,res){
-    res.send("<h1>Hello World</h1>");
+    res.render("hello.ejs");
 })
+
+app.get("/lover/:thing",function(req,res){
+   var tvar = req.params.thing;
+   res.render("love.ejs",{thingVar:tvar});
+});
 
 app.listen(process.env.PORT,process.env.IP,function(){
    console.log("Server Started"); 
