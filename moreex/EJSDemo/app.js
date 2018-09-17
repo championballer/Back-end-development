4de68a5f11e9a64a30e38160089632e1198f9172
Shiv Kumar <championballer08@gmail.com>
2018-09-17 12:58:35 +0000

    added control flow

--- moreex/EJSDemo/app.js
+++ moreex/EJSDemo/app.js
@@ -10,6 +10,16 @@ app.get("/lover/:thing",function(req,res){
    res.render("love.ejs",{thingVar:tvar});
 });
 
+app.get("/posts",function(req,res){
+   var posts = [
+       {title: "Post 1", author:"Sussy"},
+       {title: "Post 2", author:"Harry"},
+       {title: "Post 3", author:"Bob"}
+       ]
+       
+    res.render("posts.ejs",{posts:posts});
+});
+
 app.listen(process.env.PORT,process.env.IP,function(){
    console.log("Server Started"); 
 });
